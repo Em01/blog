8af389aac3f12e9fe84479f81052822b514fea3e
Em01 <emma_williams1@hotmail.co.uk>
2014-12-29 14:37:49 +0000

    post controller errors fixed and validations worked on

--- blog/app/controllers/posts_controller.rb
+++ blog/app/controllers/posts_controller.rb
@@ -17,17 +17,14 @@ class PostsController < ApplicationController
 		end
 	end
 
-		@post.save
-
-		redirect_to @post
-	end
-
 	def show
 		@post = Post.find(params[:id])
 	end
 
 	private
+
 	def post_params
 		params.require(:post).permit(:title, :body)
 	end
+
 end
