c200979df46415368587849aa80b65c44a666fab
Em01 <emma_williams1@hotmail.co.uk>
2014-12-29 14:25:19 +0000

    added some validations

--- blog/app/controllers/posts_controller.rb
+++ blog/app/controllers/posts_controller.rb
@@ -4,10 +4,19 @@ class PostsController < ApplicationController
 	end
 
 	def new
+		@post = Post.new
 	end
 
 	def create
 		@post = Post.new(post_params)
+
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
+	end
+
 		@post.save
 
 		redirect_to @post
